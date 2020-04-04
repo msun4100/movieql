d3282c87598338718c9c3fc68e9289f62a4e740c
msun4100 <msun4100@gmail.com>
2020-04-05 01:21:13 +0900

    Update: Use Axios requests

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,11 @@
-import { getMovies } from "./db";
+import { getMovies, getMovie } from "./db";
 
 const resolvers = {
   Query: {
-    movies: (_, { rating, limit }) => getMovies(limit, rating)
-  }
+    movies: (_, { rating, limit }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    getSuggestions: (_, { id }) => getMovies(id),
+  },
 };
 
 export default resolvers;
